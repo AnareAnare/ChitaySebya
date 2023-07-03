6e339bfc96c1964993e8e4a43fbd7ad9fe598705
unknown <C:\Users\Administrator\AppData\Roaming\The Bat!>
2023-07-03 18:04:11 +0300

    monday

--- chitaysebya.ru/chitaysebya.ru/index.js
+++ chitaysebya.ru/chitaysebya.ru/index.js
@@ -11,4 +11,8 @@ document.addEventListener('DOMContentLoaded', () => {
             clickable: 'true',
         },
     });
+
+    const tel = document.getElementById('tel');
+    const im = new Inputmask("+7 (999)-999-99-99");
+    im.mask(tel);
 });
